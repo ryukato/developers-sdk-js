fe33ca59c1d6eba892c2f60e11fc89717d2a5997
yoonyoulyoo <yoonyoul@linecorp.com>
2021-04-14 09:03:18 +0900

    fix: export objects

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
 export * from './lib/http-client-base';
 export * from './lib/request';
-export * from './lib/signature-generator';
+export * from './lib/response';
+export * from './lib/request-body-flattener';
